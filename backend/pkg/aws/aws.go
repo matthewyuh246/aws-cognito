fc1a5883ff60da9b1cf3a16bfd434c89b52d81b0
matthewyuh246 <matthewyuh246@gmail.com>
2025-07-16 20:18:44 +0900

    cognitoの環境変数読み込み

--- backend/pkg/aws/aws.go
+++ backend/pkg/aws/aws.go
@@ -1,5 +1,7 @@
 package aws
 
+import "github.com/matthewyuh246/aws-cognito/pkg/utils"
+
 type CognitoConfig struct {
 	Port string
 	AWSRegion string
@@ -8,3 +10,12 @@ type CognitoConfig struct {
 	JWTSecret string
 }
 
+func loadCognitoConfig() *CognitoConfig {
+	return &CognitoConfig{
+		Port: utils.GetEnv("PORT", "8080"),
+		AWSRegion: utils.GetEnv("AWS_REGION", ""),
+		UserPoolID: utils.GetEnv("USER_POOL_ID", ""),
+		UserPoolClientID: utils.GetEnv("USER_POOL_CLIENT_ID", ""),
+		JWTSecret: utils.GetEnv("JWT_SECRET", "your-secret-key"),
+	}
+}
