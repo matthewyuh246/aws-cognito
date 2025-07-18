0567d1ddf9dcc321931ba8cdf2ab629bec18de08
matthewyuh246 <matthewyuh246@gmail.com>
2025-07-18 21:05:00 +0900

    AuthTokens, UserClaims構造体

--- backend/internal/domain/user.go
+++ backend/internal/domain/user.go
@@ -18,3 +18,20 @@ type User struct {
 	UpdatedAt time.Time `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 }
+
+type AuthTokens struct {
+	AccessToken string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	IdToken string `json:"id_token"`
+	ExpiresIn int `json:"expires_in"`
+}
+
+type UserClaims struct {
+	UserID uint `json:"user_id"`
+	Email string `json:"email"`
+	Username string `json:"username"`
+	Name string `json:"name"`
+	Picture string `json:"picture"`
+	Provider string `json:"provider"`
+	Exp int64 `json:"exp"`
+}
