e84c261ad4e13c783087ab559db6770c73e888b0
matthewyuh246 <matthewyuh246@gmail.com>
2025-07-19 22:28:01 +0900

    network.tf

--- infra/network.tf
+++ infra/network.tf
@@ -1,23 +1,23 @@
 # vpc
 resource "aws_vpc" "main" {
-    cidr_block = local.vpc_cidr_block
-    enable_dns_support = true
-    enable_dns_hostnames = true
-    tags = {
-        Name = "${var.project}-${var.environment}-vpc"
-    }
+  cidr_block           = local.vpc_cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.project}-${var.environment}-vpc"
+  }
 }
 
 # subnet
 resource "aws_subnet" "public" {
-    count = length(local.public_subnet_cidr_blocks)
-    vpc_id = aws_vpc.main.id
-    cidr_block = element(local.public_subnet_cidr_blocks, count.index)
-    availability_zone = element(local.availability_zones, count.index)
+  count             = length(local.public_subnet_cidr_blocks)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(local.public_subnet_cidr_blocks, count.index)
+  availability_zone = element(local.availability_zones, count.index)
 
-    tags = {
-        Name = "${var.project}-${var.environment}-public-subnet0${count.index}"
-    }
+  tags = {
+    Name = "${var.project}-${var.environment}-public-subnet0${count.index}"
+  }
 }
 
 resource "aws_subnet" "private" {
