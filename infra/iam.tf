4b32d53cd1975bee9a285e18f21accb6eb80a695
matthewyuh246 <matthewyuh246@gmail.com>
2025-07-21 04:34:54 +0900

    iam

---
+++ infra/iam.tf
@@ -0,0 +1,33 @@
+# ===========================================
+# Cognito Outputs
+# ===========================================
+
+output "user_pool_id" {
+  description = "Cognito User Pool ID"
+  value       = aws_cognito_user_pool.main.id
+}
+
+output "user_pool_client_id" {
+  description = "Cognito User Pool Client ID"
+  value       = aws_cognito_user_pool_client.main.id
+}
+
+output "identity_pool_id" {
+  description = "Cognito Identity Pool ID"
+  value       = aws_cognito_identity_pool.main.id
+}
+
+output "user_pool_endpoint" {
+  description = "Cognito User Pool Endpoint"
+  value       = aws_cognito_user_pool.main.endpoint
+}
+
+output "user_pool_domain" {
+  description = "Cognito User Pool Domain"
+  value       = aws_cognito_user_pool_domain.main.domain
+}
+
+output "cognito_domain_url" {
+  description = "Cognito Domain URL"
+  value       = "https://${aws_cognito_user_pool_domain.main.domain}.auth.${var.aws_region}.amazoncognito.com"
+} 
