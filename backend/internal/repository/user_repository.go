1fca01f3dd8372c2a3e226516f257d4d8f55fc14
matthewyuh246 <matthewyuh246@gmail.com>
2025-07-28 23:30:53 +0900

    createUser

--- backend/internal/repository/user_repository.go
+++ backend/internal/repository/user_repository.go
@@ -1,5 +1,24 @@
 package repository
 
-type UserRepository interface {
-	
+import (
+	"context"
+
+	"github.com/matthewyuh246/aws-cognito/internal/domain"
+	"gorm.io/gorm"
+)
+
+type IUserRepository interface {
+	createUser(ctx context.Context, user *domain.User) error
+}
+
+type userRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) IUserRepository {
+	return &userRepository{db:db}
+}
+
+func (r *userRepository) createUser(ctx context.Context, user *domain.User) error {
+	return r.db.WithContext(ctx).Create(user).Error
 }
