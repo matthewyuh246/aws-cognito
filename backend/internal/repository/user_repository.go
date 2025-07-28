ec9f78762f9b320c0be32076ea29c7746ded3bef
matthewyuh246 <matthewyuh246@gmail.com>
2025-07-28 23:50:58 +0900

    getUserByEmail

--- backend/internal/repository/user_repository.go
+++ backend/internal/repository/user_repository.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"errors"
 
 	"github.com/matthewyuh246/aws-cognito/internal/domain"
 	"gorm.io/gorm"
@@ -9,6 +10,7 @@ import (
 
 type IUserRepository interface {
 	createUser(ctx context.Context, user *domain.User) error
+	getUserByEmail(ctx context.Context, email string) (*domain.User, error)
 }
 
 type userRepository struct {
@@ -21,4 +23,16 @@ func NewUserRepository(db *gorm.DB) IUserRepository {
 
 func (r *userRepository) createUser(ctx context.Context, user *domain.User) error {
 	return r.db.WithContext(ctx).Create(user).Error
+}
+
+func (r *userRepository) getUserByEmail(ctx context.Context, email string) (*domain.User, error) {
+	var user domain.User
+	err := r.db.WithContext(ctx).Where("email = ?", email).First(&user).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
 }
